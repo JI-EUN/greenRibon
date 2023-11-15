91158762ef57a332025768ce66336a232c6240d5
JI-EUN <pangdoa_merry@daum.net>
2023-11-15 16:37:54 +0900

    지도

--- assets/js/greenRibon.js
+++ assets/js/greenRibon.js
@@ -114,4 +114,32 @@
       prevEl: ".nav-prev",
     },
   });
+
+  var mapContainer = document.getElementById("map-seoul"), // 지도를 표시할 div
+    mapOption = {
+      center: new kakao.maps.LatLng(37.5455, 126.9526), // 지도의 중심좌표
+      level: 3, // 지도의 확대 레벨
+    };
+  var map = new kakao.maps.Map(mapContainer, mapOption);
+  var markerPosition = new kakao.maps.LatLng(37.5455, 126.9526);
+  var marker = new kakao.maps.Marker({
+    position: markerPosition,
+  });
+
+  marker.setMap(map);
+
+  var mapContainer2 = document.getElementById("map-daejeon"), // 지도를 표시할 div
+    mapOption = {
+      center: new kakao.maps.LatLng(36.3285, 127.4232), // 지도의 중심좌표
+      level: 3, // 지도의 확대 레벨
+    };
+
+  // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
+  var map2 = new kakao.maps.Map(mapContainer2, mapOption);
+  var markerPosition2 = new kakao.maps.LatLng(36.3285, 127.4232);
+  var marker2 = new kakao.maps.Marker({
+    position: markerPosition2,
+  });
+
+  marker2.setMap(map2);
 })();
